f79190794e3bbcd8e2fea31f53636238f46ee7f1
marcondesjr96 <jrmoriaproducoes1996@gmail.com>
2023-07-04 15:45:38 -0300

    criando o emailConsumer

---
+++ src/main/java/com/microservices/marcondesjr/microserviceemail/consumers/EmailConsumer.java
@@ -0,0 +1,25 @@
+package com.microservices.marcondesjr.microserviceemail.consumers;
+
+import com.microservices.marcondesjr.microserviceemail.domain.Email;
+import com.microservices.marcondesjr.microserviceemail.dtos.EmailDto;
+import com.microservices.marcondesjr.microserviceemail.service.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen (@Payload EmailDto emailDto){
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email);
+        emailService.sendEmail(email);
+        System.out.println("Email Status: " + email.getStatusEmail().toString());
+    }
+}
